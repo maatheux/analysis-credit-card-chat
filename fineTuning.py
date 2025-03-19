e136cf6afc27edd92fd53f1f57aacd57041cd029
Matheus Lima <matheusmarinho.lima@hotmail.com>
2025-03-18 23:50:08 -0300

    chore: init

---
+++ fineTuning.py
@@ -0,0 +1,31 @@
+﻿from transformers import LlamaForCausalLM, LlamaTokenizer, Trainer, TrainingArguments, AutoModelForCausalLM, AutoTokenizer
+import torch
+import json
+
+
+def load_dataset():
+    with open(r"./PreDatasets/CreditCard.json", "r", encoding="utf-8-sig") as f:
+        dataset = json.load(f)
+    
+    return dataset
+
+
+# def data_processing(data):
+#     inputs = tokenizer([x["chat"] for x in data], truncation=True, padding=True, max_length=512)
+#     labels = tokenizer([x["result"] for x in data], truncation=True, padding=True, max_length=512)
+# 
+#     return {"input_ids": inputs["input_ids"], "attention_mask": inputs["attention_mask"], "labels": labels["input_ids"]}
+
+
+
+
+if __name__ == "__main__":
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
+    model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B") 
+    
+    # dataset = load_dataset()
+    
+    
+
+
+
